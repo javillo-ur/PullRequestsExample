4b52f425d613b1301b7460df6ecc86e10d2ab4bd
adbaldel <adbaldel@unirioja.es>
2026-02-09 15:46:03 +0100

    Modificar clase App para llamar al método CUASI de javillo y adbaldel

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Recolector de CUASIs");
+        javillo j = new javillo();
+        adbaldel a = new adbaldel();
+
+        j.cuasi();
+        a.cuasi();
     }
 }
